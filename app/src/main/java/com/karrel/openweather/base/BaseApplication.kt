6039e6e03d333ce8e65502408ac7d9aefd9275af
eastarj <eastarj@gmail.com>
2019-12-09 23:06:21 +0900

    Crashlytics 추가

--- app/src/main/java/com/karrel/openweather/base/BaseApplication.kt
+++ app/src/main/java/com/karrel/openweather/base/BaseApplication.kt
@@ -2,6 +2,9 @@ package com.karrel.openweather.base
 
 import android.app.Application
 import android.content.Context
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.analytics.FirebaseAnalytics
+import io.fabric.sdk.android.Fabric
 import team.tuna.openweather.constant.appContext
 
 class BaseApplication : Application() {
@@ -13,6 +16,8 @@ class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+//        Fabric.with(this, Crashlytics())
+        Crashlytics.log("시작됨")
 
         instance = this
         appContext = applicationContext
